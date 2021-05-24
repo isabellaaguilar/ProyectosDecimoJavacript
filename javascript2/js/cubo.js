11d416860285dc7c5f3aa49e085c0bb936ddcfe4
Isabella Aguilar <ia514648@gmail.com>
2021-05-24 13:48:26 -0600

    Nuevo commit cuadrado y cubo en javascript

--- javascript2/js/cubo.js
+++ javascript2/js/cubo.js
@@ -1,12 +1,12 @@
-function cuadrado() {
+function cubo() {
     let resultado = 0,
         result = 0,
         monto = 0;
     resultado = document.getElementById("res");
     monto = document.getElementById("n1");
-    monto.style.background = "#a9a955";
-    result = monto.value * monto.value;
-    resultado.style.color = "#00ff55";
+    monto.style.background = "#FF33E9";
+    result = monto.value * monto.value * monto.value;
+    resultado.style.color = "#FF0000 ";
     resultado.innerHTML = result;
 
 }
