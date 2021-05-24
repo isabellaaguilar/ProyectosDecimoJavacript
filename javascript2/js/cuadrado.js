d05d72518a2a002a74fc4a1e6ad496db23851e08
Isabella Aguilar <ia514648@gmail.com>
2021-05-24 08:34:21 -0600

    cambios

---
+++ javascript2/js/cuadrado.js
@@ -0,0 +1,10 @@
+function cuadrado() {
+    let resultado = 0,
+        result = 0,
+        monto = 0;
+    resultado = document.getElementById("res");
+    monto = document.getElementById("n1");
+    result = monto.value * monto.value;
+    alert(result);
+
+}
