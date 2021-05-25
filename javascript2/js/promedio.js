98b29b950501c063060bcaeb18cdf4786fee3144
Isabella Aguilar <ia514648@gmail.com>
2021-05-25 08:18:27 -0600

    cambios ma;ana 25 de mayo

--- javascript2/js/promedio.js
+++ javascript2/js/promedio.js
@@ -17,20 +17,26 @@ function vaciar3() {
 }
 
 function dividir() {
+    let hayError = false
     let dividir = 0;
     if (isNaN(cajaTexto[0].value)) {
         alert("por favor ingrese datos numericos");
         vaciar() //esto vacia la cajita al darle aceptar
+        hayError = true
     }
+
     if (isNaN(cajaTexto[1].value)) {
         alert("por favor ingrese datos numericos");
         vaciar2() //esto vacia la cajita al darle aceptar
-        if (isNaN(cajaTexto[2].value)) {
-            alert("por favor ingrese datos numericos");
-            vaciar3() //esto vacia la cajita al darle aceptar
-        }
-    } else {
+        hayError = true
+    }
+    if (isNaN(cajaTexto[2].value)) {
+        alert("por favor ingrese datos numericos");
+        vaciar3() //esto vacia la cajita al darle aceptar
+        hayError = true
+    }
 
+    if (!hayError) {
         dividir = (parseInt(cajaTexto[0].value) + parseInt(cajaTexto[1].value) + parseInt(cajaTexto[2].value)) / 3;
         //aqui estoy haciendo la dividira despues de la caja texto la primera es 0 porque es la primera la segunda es 1  por que es la segunda
         document.getElementById('resultado').innerHTML = dividir.toFixed(1);
