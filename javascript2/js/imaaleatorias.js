45b19248d35003cc79cd4efd32dba3aadab3ffcc
Isabella Aguilar <ia514648@gmail.com>
2021-05-26 17:10:04 -0600

    Cmbios de la tarde

---
+++ javascript2/js/imaaleatorias.js
@@ -0,0 +1,9 @@
+let img = document.createElement("img"), //esto crea una etiqueta en este caso la etiqueta img
+    divimagenes = document.queryselector('#n2'),
+    num = Math.round(Math.round() * (10 - 1) + 1);
+
+function buscarImagen() {
+    img.src = ` autos/${1}.jpg `;
+    divimagenes.apped(img);
+
+}
